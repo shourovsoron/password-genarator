c67193caf31760cf384016677c9de4d206457990
Shourov Soron <shourovhossainsoron@gmail.com>
2024-05-09 11:24:25 +0600

    second commit

--- main.js
+++ main.js
@@ -1,57 +1,32 @@
-const createBtn = document.querySelector(".create-note");
-const notesConatiner = document.querySelector(".notes-container");
 
+const display = document.getElementById("display");
+const uppercase = "ABCDEFGHIJKLMNOPQUSTUVWXYZ";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const character = "!@#$%^&*()-_+=/*~{[]}"
 
-checkStorage();
+const allchar = uppercase + lowercase + number + character;
+let password = "";
+const leanth = 12;
 
-function createNote(){
-    const note=document.createElement("div");
-    note.className="note"
-    const noteInput=document.createElement("p");
-    noteInput.setAttribute("contenteditable", "true")
-    noteInput.className="note-input";
-    const deleteNote=document.createElement("img");
-    deleteNote.src="./images/delete.png"
-    note.appendChild(noteInput);
-    note.appendChild(deleteNote);
-    if(!notesConatiner.innerHTML==""){
-        notesConatiner.insertBefore(note, notesConatiner.children[0]);
-    }else{
-        notesConatiner.appendChild(note)
+function generatePass(){
+    password = "";
+    password += uppercase[Math.floor(Math.random() * uppercase.length)];
+    password += lowercase[Math.floor(Math.random() * lowercase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += character[Math.floor(Math.random() * character.length)];
+    while(password.length<leanth){
+        password += allchar[Math.floor(Math.random() * allchar.length)];
     }
-    updateStorage()
-}
-function updateStorage(){
-    localStorage.setItem("notes", notesConatiner.innerHTML);
+    display.value = password;
 }
 
-function checkStorage(){
-    notesConatiner.innerHTML=localStorage.getItem("notes");
+function copypass(){
+    if(!password.length==0){
+        display.select();
+        document.execCommand("copy");
+        display.value = password + " Copied!";
+
+    }
 }
 
-createBtn.addEventListener("click" ,createNote);
-
-
-
-notesConatiner.addEventListener("click" , (e)=>{
-    if(e.target.tagName === "IMG"){
-        e.target.parentElement.remove();
-        updateStorage()
-    }else if(e.target.tagName === "P"){
-    
-        const allNotes=document.querySelectorAll(".note-input");
-        allNotes.forEach(note => {
-            note.onkeyup =  updateStorage;
-        })
-
-
-    }
-
-})
-
-document.addEventListener("keydown", event=>{
-    if(event.key=="Enter"){
-        document.execCommand("InsertLineBreak");
-        event.preventDefault();
-    }
-})
